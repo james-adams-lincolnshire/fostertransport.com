7ac1baa45d372b51b3d07adea039caf3399ac6ad
james-adams-lincolnshire <mail.jamesadams@gmail.com>
2017-09-22 21:01:16 +0100

    Admin UI makeover

--- static/javascript/site.js
+++ static/javascript/site.js
@@ -10,9 +10,10 @@ var app = {
 
 		tool.init(); // Must be first
 		menu.events();
+		subMenu.events();
 		loading.events();
 	}
-}
+};
 
 var tool = {
 	init: function () {
@@ -37,7 +38,7 @@ var tool = {
 		xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
 		//.bind ensures that this inside of the function is the XHR object.
-		xhr.onload = callback.bind(xhr); 
+		xhr.onreadystatechange  = callback.bind(xhr); 
 
 		//All preperations are clear, send the request!
 		xhr.send(params);
@@ -65,6 +66,7 @@ var tool = {
 			autoScrollEditorIntoView: true,
 			wrap: true,
 			theme: "ace/theme/monokai",
+			fontSize: '15px',
 			showPrintMargin: true,
 			mode: "ace/mode/html"
 		});
@@ -73,6 +75,7 @@ var tool = {
 			maxLines: 1000,
 			autoScrollEditorIntoView: true,
 			theme: "ace/theme/monokai",
+			fontSize: '15px',
 			showPrintMargin: true,
 			mode: "ace/mode/css"
 		});
@@ -81,19 +84,36 @@ var tool = {
 			maxLines: 1000,
 			autoScrollEditorIntoView: true,
 			theme: "ace/theme/monokai",
+			fontSize: '15px',
 			showPrintMargin: true,
 			mode: "ace/mode/javascript"
 		});
+	},
+	forceRedraw: function (element) {
+		if (!element) { return; }
+
+		var n = document.createTextNode(' ');
+		var disp = element.style.display;  // don't worry about previous display style
+
+		element.appendChild(n);
+		element.style.display = 'none';
+
+		setTimeout(function () {
+			element.style.display = disp;
+			n.parentNode.removeChild(n);
+		}, 20); // you can play with this timeout to make it as short as possible
 	}
-}
+};
 
 var menu = {
 	mobile: {
 		toggle: function() {
 			var header = document.getElementById('ft-header');
 			var background = document.getElementById('ft-header-background');
+			var nav = document.getElementsByClassName('ft-navigation');
 
 			header.classList.toggle('ft-open');
+			nav[0].classList.toggle('ft-open');
 			background.classList.toggle('ft-menu-open');
 
 			setTimeout(function() { // Allow menu closing.
@@ -114,9 +134,18 @@ var menu = {
 			saveBtn.disabled = true;
 			saveBtn.value = '...';
 			
-			var callback = function(e) {
-				e.value = 'Save';
-				e.disabled = false;
+			var callback = function(btn, xhr) {
+				if (xhr.target.readyState === XMLHttpRequest.DONE && xhr.target.status === 200) {
+					btn.value = 'Save';
+					btn.disabled = false;
+				} else if (xhr.target.readyState === XMLHttpRequest.DONE && xhr.target.status !== 200) {
+					btn.value = 'Error';
+					
+					setTimeout(function () {
+						btn.value = 'Save';
+						btn.disabled = false;
+					}, 1500);
+				}
 			};
 			
 			document.getElementById('html').value = tool.htmlEditor.getValue();
@@ -133,14 +162,18 @@ var menu = {
 				
 				quickSaveBtn.addEventListener('click', function(e) {
 					this.disabled = true;
-					this.classList.add('ft-loading-animation');
+					this.classList.add('ft-loading');
 					
 					var formId = this.getAttribute("data-form-id");
 					
-					var callback = function(clickedBtn) {
-						console.log('clicked!!');
-						//clickedBtn.classList.remove('ft-loading-animation');
-						clickedBtn.disabled = false;
+					var callback = function (btn, xhr) {
+						btn.classList.remove('ft-loading');
+						
+						if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+							btn.disabled = false;
+						} else if (xhr.readyState === XMLHttpRequest.DONE && xhr.status !== 200) {
+							btn.value = 'Error';
+						}
 					};
 					
 					menu.editSection.quickSave.captureHtml(formId);
@@ -165,10 +198,90 @@ var menu = {
 			
 			var editSectionForm = document.getElementById(formId);
 			
-			tool.ajaxPost(editSectionForm, function () {
-				if (callback) callback(clickedBtn);
+			tool.ajaxPost(editSectionForm, function (e) {
+				if (callback) callback(clickedBtn, e);
 			});
 		}
+	}
+};
+
+var subMenu = {
+	mobile: {
+		toggle: function() {
+			var subMenuElem = document.getElementById('ft-sub-menu');
+			var subMenuBtn = document.getElementById('ft-sub-menu-btn');
+			subMenuElem.classList.toggle('ft-open');
+			subMenuBtn.classList.toggle('ft-pulse');
+		},
+		events: function() {
+			var subMenuBtn = document.getElementById('ft-sub-menu-btn');
+			if (subMenuBtn) subMenuBtn.addEventListener('click', subMenu.mobile.toggle);
+			
+			var doneBtn = document.getElementById('ft-done-btn');
+			if (doneBtn) {
+					doneBtn.addEventListener('click', function(e) {
+						e.preventDefault();
+						loading.toggle();
+						doneBtn.disabled = true;
+						
+						var callback = function(btn, xhr) {
+							if (xhr.target.readyState === XMLHttpRequest.DONE && xhr.target.status === 200) {
+								var newLocation = window.location.protocol + "//" +
+															   window.location.host +
+															   btn.getAttribute('href');
+															   
+								window.location = newLocation;
+							} else if (xhr.target.readyState === XMLHttpRequest.DONE && xhr.target.status !== 200) {
+								console.log('Error details: ' + xhr.target.statusText);
+								alert('Failed To Save');
+								loading.toggle();
+								btn.disabled = false;
+							}
+						};
+						
+						document.getElementById('html').value = tool.htmlEditor.getValue();
+						document.getElementById('css').value = tool.cssEditor.getValue();
+						document.getElementById('javascript').value = tool.jsEditor.getValue();
+						
+						menu.editSection.submit('ft-edit-section-form', e, callback, doneBtn);
+				});
+			}
+			
+			var createBtn = document.getElementById('ft-create-btn');
+			var createSubMenuBtn = document.getElementById('ft-create-sub-menu-btn');
+			var createCallback = function(e) {
+				e.preventDefault();
+				loading.toggle();
+				createBtn.disabled = true;
+				
+				var callback = function(btn, xhr) {
+					if (xhr.target.readyState === XMLHttpRequest.DONE && xhr.target.status === 200) {
+						var newLocation = window.location.protocol + "//" +
+													   window.location.host +
+													   btn.getAttribute('data-complete-url');
+													   
+						window.location = newLocation;
+					} else if (xhr.target.readyState === XMLHttpRequest.DONE && xhr.target.status !== 200) {
+						console.log('Error details: ' + xhr.target.statusText);
+						alert('Failed To Save');
+						loading.toggle();
+						btn.disabled = false;
+					}
+				};
+				
+				document.getElementById('html').value = tool.htmlEditor.getValue();
+				document.getElementById('css').value = tool.cssEditor.getValue();
+				document.getElementById('javascript').value = tool.jsEditor.getValue();
+				
+				menu.editSection.submit('ft-create-section-form', e, callback, createBtn);
+			};
+			
+			if (createBtn) createBtn.addEventListener('click', createCallback);
+			if (createSubMenuBtn) createSubMenuBtn.addEventListener('click', createCallback);
+		}
+	},
+	events: function() {
+		subMenu.mobile.events();
 	}
 };
 
@@ -185,7 +298,10 @@ var loading = {
 		var links = document.getElementsByTagName('a');
 
 		for (var i = 0; i < links.length; i++)
-			if (!links[i].classList.contains('ft-no-loading') && !links[i].hasAttribute('target') && /(?:^|\s)ft-\S*(?:$|\s)/.test(links[i].className))
+			if (!links[i].classList.contains('ft-no-loading') &&
+				!links[i].hasAttribute('target') && 
+				(/(?:^|\s)ft-\S*(?:$|\s)/.test(links[i].className) ||
+				 !links[i].className))
 				links[i].addEventListener('click', loading.toggle);
 	}
-}
+};
